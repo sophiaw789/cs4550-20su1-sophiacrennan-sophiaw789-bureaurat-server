d81f0235109f96735cf1c4c66f7ff1ae1876bf40
sophiaw789 <sophialiwon@gmail.com>
2020-06-17 12:42:48 -0400

    removed JPA

--- src/main/java/com/example/cs455020su1sophiacrennansophiaw789bureauratserver/repositories/UserRepository.java
+++ src/main/java/com/example/cs455020su1sophiacrennansophiaw789bureauratserver/repositories/UserRepository.java
@@ -1,3 +1,4 @@
+/*
 package com.example.cs455020su1sophiacrennansophiaw789bureauratserver.repositories;
 
 import com.example.cs455020su1sophiacrennansophiaw789bureauratserver.models.User;
@@ -12,3 +13,4 @@ public interface UserRepository extends CrudRepository<User, Integer> {
     @Query("SELECT user FROM User user WHERE user.username=:username AND user.password=:password")
     User findUserByCredentials(@Param("username") String username, @Param("password") String password);
 }
+*/
