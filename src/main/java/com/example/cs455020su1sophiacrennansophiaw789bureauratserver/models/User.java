551f9ac9392f2ea53bbc7b96a626cc2936529938
sophiaw789 <sophialiwon@gmail.com>
2020-06-17 05:13:29 -0400

    first commit

---
+++ src/main/java/com/example/cs455020su1sophiacrennansophiaw789bureauratserver/models/User.java
@@ -0,0 +1,55 @@
+package com.example.cs455020su1sophiacrennansophiaw789bureauratserver.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String password;
+    private String email;
+    private Boolean admin;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        this.admin = admin;
+    }
+}
